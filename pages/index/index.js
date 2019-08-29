92b2ab6f4b6d81cce6327312eeb54af5136f96c2
daishuaiqing <daishuaiqing@163.com>
2019-08-29 18:47:31 +0800

    首页样式存在Big，重新调整，实现导航切换

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,6 +7,25 @@ Page({
     isShowCart: false,//是否显示购物车商品
     isShowCartBar: true,//是否显示购物车的Bar
     cart: [],//购物车
+    currentTab: 0,
+    menuList: [
+      '好吃推荐',
+      '新品尝鲜',
+      '所有果品',
+      '鲜果现切',
+      '招牌果品',
+      '西瓜密瓜',
+      '葡提浆果',
+      '桃李杏枣',
+      '纱卡拉卡',
+      '私密马赛'
+    ],
+  },
+  //菜单切换
+  navbarTap: function(e){
+    this.setData({
+      currentTab: e.currentTarget.dataset.index
+    })
   },
   //事件处理函数
   bindViewTap: function() {
@@ -15,16 +34,16 @@ Page({
     })
   },
   onLoad: function () {
-    this.loadGoodView();
+    //this.loadGoodView();
   },
   //适配商品列表的View高度
-  loadGoodView(){
-    let windowHeight = wx.getSystemInfoSync().windowHeight // 屏幕的高度
-    let windowWidth = wx.getSystemInfoSync().windowWidth // 屏幕的宽度
-    this.setData({
-      height: windowHeight * 750 / windowWidth - 92
-    })
-  },
+  // loadGoodView(){
+  //   let windowHeight = wx.getSystemInfoSync().windowHeight // 屏幕的高度
+  //   let windowWidth = wx.getSystemInfoSync().windowWidth // 屏幕的宽度
+  //   this.setData({
+  //     height: windowHeight * 750 / windowWidth - 92
+  //   })
+  // },
   //点击购物车图标显示加入购物车的商品
   openCart(){
     if(this.data.isShowCart){
